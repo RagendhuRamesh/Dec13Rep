9d60588e2b58246c5484c8ec9cb3e63870cdda89
RagendhuRamesh <ragendhuram@gmail.com>
2023-12-14 14:49:10 +0530

    Add project files.

---
+++ Dec13Console/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dec13Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's up");
+            Console.WriteLine("This is Ragendhu");
+            Console.ReadLine();
+        }
+    }
+}
